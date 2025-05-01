13bfb187ee6339bb956fa78d12fff9411c4821c1
Phong9383438 <23521170@gm.uit.edu.vn>
2025-05-01 17:32:06 +0700

    Update MyLogin(product), change and add product data

--- Webdev_project/Models/Product.cs
+++ Webdev_project/Models/Product.cs
@@ -3,7 +3,8 @@
     public class Product
     {
         public string Name { get; set; }
-        public string Decription { get; set; }
+        public string Description { get; set; }
+        public List<string> Color { get; set; }
         public int Price { get; set; }
         public List<string> ImageURL { get; set; }
         public Dictionary<string, string> Detail { get; set; }
