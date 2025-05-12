f3a06515c7878027eb0fc3cd8a32797eb4f90d3e
Phuc179 <23521218@gm.uit.edu.vn>
2025-05-12 10:05:34 +0700

    thêm ShoppingCart trong phần view , liên kết trong _Layout.cshtml

---
+++ Webdev_project/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webdev_project.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Ví dụ: Lấy dữ liệu giỏ hàng từ cơ sở dữ liệu hoặc session
+            var cartItems = new List<string> { "Sản phẩm 1", "Sản phẩm 2", "Sản phẩm 3" };
+
+            // Truyền dữ liệu sang view
+            return View(cartItems);
+        }
+    }
+}
